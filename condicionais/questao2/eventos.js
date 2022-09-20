094a1b1ce389e54855c81fe1d5592dd89458147e
Igor-XV <igor.1103r@gmail.com>
2022-09-20 16:18:53 -0300

    condicional 02 resolvida

---
+++ condicionais/questao2/eventos.js
@@ -0,0 +1,12 @@
+let nome = "igor";
+let EstadoCivil = "casado";
+let Sexo = "F";
+
+if ( Sexo === "F" && EstadoCivil === "casado") {
+       
+    prompt(" Digite o Tempo de Casado");
+
+} else {
+    
+    prompt('bora Tc ?')
+}
