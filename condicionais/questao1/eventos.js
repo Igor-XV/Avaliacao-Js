49e7cae8b2f22aa19438311e2e96fc794cad0c6e
Igor-XV <igor.1103r@gmail.com>
2022-09-20 15:42:41 -0300

    Condicional 01 resolvida

---
+++ condicionais/questao1/eventos.js
@@ -0,0 +1,13 @@
+let a = 6;
+let b = 1;
+let c = 10;
+
+if ( a + b < c) {
+    c < a + b ;
+    document.write (" A soma de a + b e menor que C: ", c);
+} else {
+    c > a + b;
+    document.write("A soma de a + b e maior que C: ", c)
+}
+
+//resolvido 
