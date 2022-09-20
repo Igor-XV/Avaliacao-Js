966384eb4c1f1e51a16576380ad99d95989257c4
Igor-XV <igor.1103r@gmail.com>
2022-09-20 18:20:16 -0300

    condicional 05 ta na mao

---
+++ condicionais/questao05/eventos.js
@@ -0,0 +1,12 @@
+let Numero = -999999999999999;
+
+
+if (Numero >=0){
+    let Resultado = Numero * 2;
+    document.write("Resultado" , Resultado)
+
+} else {
+    let Negativo = Numero *3;
+        document.write("Negativo" , Negativo)
+
+    };
